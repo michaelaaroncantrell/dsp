70029df5feb9d8d4e2e24e04d24d8a9e0fa23304
michaelaaroncantrell <michaelaaroncantrell@gmail.com>
2016-12-07 16:31:12 -0800

    Answered all questions

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,6 +1,12 @@
-# The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+f=open("football.csv","r")
+xs=f.readlines()
+f.close()
+d=0
+index=0
+for i in range(1,len(xs)):
+    if d>(int(xs[i].split(',')[5])-int(xs[i].split(',')[6])):
+        d=int(xs[i].split(',')[5])-int(xs[i].split(',')[6])
+        index=i
+return xs[index].split(',')[0]
+#I interpreted the question to be asking for the smallest difference (including negatives), not in absolute values.
 
